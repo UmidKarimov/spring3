061c39df898ff91890bdd09dad40954354e6c502
Umid Karimov <Umid.Karimov@unibank.az>
2021-11-26 18:09:32 +0400

    init

---
+++ spring3/src/main/java/az/spring3/spring/HelloController.java
@@ -0,0 +1,15 @@
+package az.spring3.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String init (){
+        return "hello.html";
+    }
+}
