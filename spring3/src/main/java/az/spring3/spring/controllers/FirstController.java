59b359adb66cb876543670a7ae52a990462c16ae
Umid Karimov <karimovumid95@gmail.com>
2021-11-27 23:40:09 +0400

    c-m

--- spring3/src/main/java/az/spring3/spring/controllers/FirstController.java
+++ spring3/src/main/java/az/spring3/spring/controllers/FirstController.java
@@ -3,7 +3,6 @@ package az.spring3.spring.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,4 +23,5 @@ public class FirstController {
     public String sayGoodBye() {
         return "first/goodbye";
     }
+
 }
