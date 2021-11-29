efdd806ef2d3e29bb11e9b1e1fa3eeddb3804c91
Umid Karimov <Umid.Karimov@unibank.az>
2021-11-29 22:13:33 +0400

    add SecondController

--- spring3/src/main/java/az/spring3/spring/controllers/SecondController.java
+++ spring3/src/main/java/az/spring3/spring/controllers/SecondController.java
@@ -3,32 +3,25 @@ package az.spring3.spring.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/first")
-public class FirstController {
+@RequestMapping("/second")
+public class SecondController {
 
     @GetMapping
-    public ResponseEntity init() {
-        try {
+    public ResponseEntity init(){
+        try{
             return ResponseEntity.ok("Second Controller ready to work!");
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             return ResponseEntity.badRequest().body("Server is not working!");
         }
     }
 
-
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "first/hello";
+    @GetMapping("/exit")
+    public String exit (){
+        return "second/exit";
     }
-
-    @GetMapping("/goodbye")
-    public String sayGoodBye() {
-        return "first/goodbye";
-    }
-
-
 }
