789fa5abfcd797babb9852f92c299483c8a09142
Umid Karimov <Umid.Karimov@unibank.az>
2021-11-29 22:26:40 +0400

    add params to FirstController

--- spring3/src/main/java/az/spring3/spring/controllers/FirstController.java
+++ spring3/src/main/java/az/spring3/spring/controllers/FirstController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
@@ -13,7 +14,7 @@ public class FirstController {
     @GetMapping
     public ResponseEntity init() {
         try {
-            return ResponseEntity.ok("Second Controller ready to work!");
+            return ResponseEntity.ok("First Controller ready to work!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Server is not working!");
         }
@@ -21,7 +22,8 @@ public class FirstController {
 
 
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHello(@RequestParam(value = "name",required = false) String name, @RequestParam(value = "surname",required = false) String surname) {
+        System.out.println(name + " " + surname);
         return "first/hello";
     }
 
