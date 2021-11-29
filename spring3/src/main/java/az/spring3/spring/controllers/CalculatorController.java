a14d7c37ee54c17e78448a6871a0af94d3299d5e
Umid Karimov <Umid.Karimov@unibank.az>
2021-11-29 22:57:00 +0400

    add CalculatorController

---
+++ spring3/src/main/java/az/spring3/spring/controllers/CalculatorController.java
@@ -0,0 +1,32 @@
+package az.spring3.spring.controllers;
+
+import az.spring3.spring.services.CalculatorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/calc")
+public class CalculatorController {
+
+
+    @GetMapping
+    public ResponseEntity init() {
+        try {
+            return ResponseEntity.ok("Calculator ready to work!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Server is not working!");
+        }
+    }
+
+    @GetMapping("/operation")
+    public String calc(@RequestParam("a") int a, @RequestParam("b") int b, @RequestParam("action") String action, Model model) {
+
+        System.out.println(CalculatorService.calculate(a,b,action));
+        model.addAttribute("result", CalculatorService.calculate(a, b, action));
+        return "calculator/calc";
+    }
+}
---
+++ spring3/src/main/java/az/spring3/spring/services/CalculatorService.java
@@ -0,0 +1,26 @@
+package az.spring3.spring.services;
+
+public class CalculatorService {
+    public static double calculate(int a, int b, String action) {
+
+        double result;
+        switch (action) {
+            case "add":
+                result= a + b;
+            break;
+            case "minus":
+                result= a - b;
+            break;
+            case "multiple":
+                result= a * b;
+            break;
+            case "devide":
+                result= (double) a / (double) b;
+            break;
+            default:
+                result=0;
+            break;
+        }
+        return result;
+    }
+}
