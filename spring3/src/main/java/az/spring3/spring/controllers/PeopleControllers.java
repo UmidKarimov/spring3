7df3e9c79575cd78a2557ce815e743de36dade21
Umid Karimov <Umid.Karimov@unibank.az>
2021-12-07 09:19:01 +0400

    c

--- spring3/src/main/java/az/spring3/spring/controllers/PeopleControllers.java
+++ spring3/src/main/java/az/spring3/spring/controllers/PeopleControllers.java
@@ -2,9 +2,11 @@ package az.spring3.spring.controllers;
 
 import az.spring3.spring.DAO.PersonDAO;
 import az.spring3.spring.models.Person;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -37,7 +39,10 @@ public class PeopleControllers {
     }
 
     @PostMapping
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -49,7 +54,10 @@ public class PeopleControllers {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
--- spring3/src/main/java/az/spring3/spring/models/Person.java
+++ spring3/src/main/java/az/spring3/spring/models/Person.java
@@ -1,10 +1,23 @@
 package az.spring3.spring.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
 
     private int id;
+
+    @NotEmpty(message = "Name should not be empty!")
+    @Size(min = 2,max = 30, message = "Name size should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0,message = "Age should be greater than zero!")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email
     private String email;
 
     public Person(int id, String name, int age, String email) {
